0754c3ca12b6bc3c45e0e4ed3401cfff51169151
Oscar Suen <suen.oscar@gmail.com>
2020-06-12 14:33:53 -0400

    change scrape to use div.text

--- scrape.py
+++ scrape.py
@@ -1,4 +1,5 @@
 import os
+import re
 import requests
 from bs4 import BeautifulSoup
 
@@ -10,8 +11,9 @@ def gen():
             os.makedirs(f"txt/{year}/{month:02d}", exist_ok=True)
             for region in regions:
                 yield year, month, region
+
 def scrape():
-    errorfile = open("out/errors.csv", "w")
+    errorfile = open("out/missing.csv", "w")
     errorfile.write("year,month,region\n")
     for year, month, region in gen():
         print(f"{year}", f"{month:02d}", f"{region}", end=" ")
@@ -22,7 +24,7 @@ def scrape():
             with open(f"txt/{year}/{month:02d}/{year}-{month:02d}-{region}.txt", "w") as f:
                 f.writelines(txt)
             print("y")
-        except RuntimeError:
+        except ValueError:
             errorfile.write(f"{year},{month:02d},{region}\n")
             print("n")
     errorfile.close()
@@ -30,28 +32,40 @@ def scrape():
 def get_text(url):
     r = requests.get(url)
     if r.status_code == 404:
-        raise RuntimeError
-    assert r.status_code == 200
+        raise ValueError
+    if r.status_code != 200:
+        raise RuntimeError(f"Status Code: {r.status_code}")
     txt = r.text.replace("<br>", "").replace("<br >", "")
     soup = BeautifulSoup(txt, features="html5lib")
     div = soup.find("div", class_="col-sm-12 col-lg-8 offset-lg-1")
-    for e in div.find_all("br"):
-        e.replace_with('\n')
-    for e in div.find_all("strong"):
-        e.extract()
-    ps = div.find_all("p")[2:]
-    raw_text = list(p.string for p in ps if p.string is not None)
-    return [p.replace('\n', '').strip()+"\n" for p in raw_text]
+    lines = re.split(r"\n\s*\n", div.text)
+    lines = [re.sub(r"\s*\n\s*", " ", p.strip())+"\n" for p in lines if not re.fullmatch(r"\s*", p)]
+    return lines[3:]
 
-def check_errors(threshold=1024):
-    errorfile = open("out/smallfiles.csv", "w")
+def filesizes():
+    errorfile = open("out/filesizes.csv", "w")
+    errorfile.write("year,month,region,filesize\n")
+    for year, month, region in gen():
+        filename = f"txt/{year}/{month:02d}/{year}-{month:02d}-{region}.txt"
+        if os.path.exists(filename):
+            errorfile.write(f"{year},{month:02d},{region},{os.path.getsize(filename)}\n")
+    errorfile.close()
+
+def missings():
+    errorfile = open("out/missings.csv", "w")
     errorfile.write("year,month,region\n")
     for year, month, region in gen():
         filename = f"txt/{year}/{month:02d}/{year}-{month:02d}-{region}.txt"
-        if os.path.exists(filename) and os.path.getsize(filename) < threshold:
+        if not os.path.exists(filename):
             errorfile.write(f"{year},{month:02d},{region}\n")
     errorfile.close()
 
 if __name__ == "__main__":
-    scrape()
-    check_errors()
+    # scrape()
+    good_url_1 = "https://www.minneapolisfed.org/beige-book-reports/1970/1970-05-at"
+    good_url_2 = "https://www.minneapolisfed.org/beige-book-reports/1971/1971-05-sf"
+    good_url_3 = "https://www.minneapolisfed.org/beige-book-reports/2001/2001-09-cl"
+    bad_url_1 = "https://www.minneapolisfed.org/beige-book-reports/1971/1971-02-kc"
+    bad_url_2 = "https://www.minneapolisfed.org/beige-book-reports/1971/1971-07-ch"
+    lines = get_text(bad_url_2)
+    print(lines)
