7eaf9ae81141fe7d8fe69b35b164de9d9e15154f
Oscar Suen <suen.oscar@gmail.com>
2020-07-02 02:30:28 -0400

    cleaned up text further

--- clean.py
+++ clean.py
@@ -9,12 +9,27 @@ def clean():
         with open(filename, "r") as f:
             old_txt = f.read()
         new_txt = old_txt
-        new_txt = cleantext.clean(new_txt, fix_unicode=True, to_ascii=True, lower=False)
-        new_txt = re.sub(r"[<>~*]", "", new_txt)
-        # new_txt = new_txt.replace("&", " and ").replace("%", " percent")
+        new_txt = cleantext.clean(new_txt, fix_unicode=True, to_ascii=True, lower=False, no_urls=True)
+
+        new_txt = re.sub(r"For more information about District economic conditions,? visit: URL", "", new_txt)
+        new_txt = new_txt.replace("%-", " percent to ").replace("%", " percent")
+        new_txt = new_txt.replace(" & ", " and ")
+        # new_txt = new_txt.replace("&", " and ") # ex. R&D
         # new_txt = re.sub(r" -(?=[\d.])", " minus ", new_txt)
-        # new_txt = new_txt.replace("+", " plus ").replace("=", " equals ")
+        # new_txt = new_txt.replace("+", " plus ")
+        new_txt = new_txt.replace("=", " equals ") # only one instance
+
+        new_txt = re.sub(r"[<>~*]", "", new_txt)
+        new_txt = re.sub(r"\-\-+", " , ", new_txt)
+        new_txt = re.sub(r"\?(?=[\w])", "? ", new_txt).replace(" ?", "?")
+        new_txt = re.sub(r"\s+,", ",", new_txt).replace(",,", ",")
+        new_txt = re.sub(r"\s+\.(?=[^0-9])", " ", new_txt)
+        new_txt = new_txt.replace("...", " ")
+        new_txt = new_txt.replace("..", ".").replace(",.", ",") # ".," is legitimate
+        new_txt = new_txt.replace("[", "").replace("]", "") # parentheses are legitimate
+
         new_txt = re.sub(r"\s+", " ", new_txt)
+        new_txt = new_txt.strip()
         with open(filename, "w") as f:
             f.write(new_txt)
 
